cd1c3fb60e69b79c7f9380b01cbdae76bfc7a372
vidharsh-01 <vidharshni6@gmail.com>
2026-04-07 09:02:22 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -17,7 +17,7 @@ export const AuthProvider = ({ children }) => {
     }, []);
 
     const login = async (email, password) => {
-        const { data } = await axios.post(`${API}`/api/users/login, {
+        const { data } = await axios.post(`${API}`users/login, {
             email,
             password,
         });
@@ -27,7 +27,7 @@ export const AuthProvider = ({ children }) => {
     };
 
     const register = async (name, email, password) => {
-        const { data } = await axios.post(`${API}`/api/users/register, {
+        const { data } = await axios.post(`${API}`users/register, {
             name,
             email,
             password,
